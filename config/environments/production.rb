77ab81fb3846a32fa81e9ad99fd38c40bfdb76bd
Thiago Correia Pereira <thiago@Thiagos-iMac.local>
2026-06-21 00:37:48 -0300

    fix: assume_ssl=true para Traefik proxy e remove healthcheck labels

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
+  # Traefik faz TLS termination, entao precisamos avisar o Rails disso.
+  config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
