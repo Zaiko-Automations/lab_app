dc7bfff610b3a82ab5ccdad9aca9ec8be3b6aa07
Thiago Correia Pereira <thiago@Thiagos-iMac.local>
2026-06-21 17:39:43 -0300

    fix: adicionado user-agent para evitar WAF Cloudflare

--- app/jobs/send_callback_webhook_job.rb
+++ app/jobs/send_callback_webhook_job.rb
@@ -59,7 +59,9 @@ class SendCallbackWebhookJob < ApplicationJob
 
     http_request = Net::HTTP::Post.new(uri.path, {
       'Content-Type' => 'application/json',
-      'Accept' => 'application/json'
+      'Accept' => 'application/json',
+      'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept-Language' => 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7'
     })
     
     http_request.body = payload.to_json
